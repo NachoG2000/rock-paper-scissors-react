1eb67b0ff010428e24fd8d5d106469f6cbe16f4c
Ignacio Garcia <ignaciongarcia00@gmail.com>
2023-05-28 02:53:07 -0300

    determine winner and play again functionallity added

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,78 @@ import rock from './images/icon-rock.svg'
 import scissors from './images/icon-scissors.svg'
 
 function App() {
-  const [buttonArray, setButtonArray] = useState([{id:1, image: rock, color: "bg-red-500"}, {id:2, image: paper, color: "bg-blue-500"}, {id:3, image: scissors, color: "bg-yellow-500"}])
+  const buttonArray = [{id:1, image: rock, color: "bg-red-500"}, {id:2, image: paper, color: "bg-blue-500"}, {id:3, image: scissors, color: "bg-yellow-500"}]
   const [selectedButton, setSelectedButton] = useState(null)
   const [houseButton, setHouseButton] = useState(null)
-  // const [isHouseSelecting, setIsHouseSelecting] = useState(false)
+  const [gameMessage, setgameMessage] = useState("")
+  const [gameCounter, setGameCounter] = useState(0)
   console.log(houseButton)
 
 
-  const buttonElements = buttonArray.map(button => <PlayButton key={button.id} id={button.id} image={button.image} color={button.color} selectButton={selectButton} selectedButton={selectedButton}/>)
+  const buttonElements = buttonArray.map(button => <PlayButton key={button.id} id={button.id} image={button.image} color={button.color} clickButton={clickButton} selectedButton={selectedButton}/>)
 
-  function selectButton(id){
+  function clickButton(id){
     setSelectedButton(buttonArray.find(button => button.id === id))  
-    if(selectButton !== null){
+    if(selectedButton === null){
       setTimeout(() => {
         const randomNum = Math.floor(Math.random() * 3)
         setHouseButton(buttonArray[randomNum])
-      }, 2000);
+      }, 1000);
     }
   }
 
+  function determineWinner(){
+    if(selectedButton.id === houseButton.id){
+      // tie
+      setgameMessage("TIE")
+    }
+    else if(selectedButton.id === 1 && houseButton.id === 3){
+      // win (user: rock, pc:scissors)
+      setgameMessage("YOU WIN")
+      setGameCounter(prevState => prevState + 1)
+    }
+    else if(selectedButton.id === 3 && houseButton.id === 1){
+      // lose (user:scissors , pc:rock)
+      setgameMessage("YOU LOSE")
+      if(gameCounter > 0){
+        setGameCounter(prevState => prevState - 1)
+      }
+    }
+    else if(selectedButton.id < houseButton.id){
+      // lose (user:rock, pc:paper || user:paper, pc:scissors)
+      setgameMessage("YOU LOSE")
+      if(gameCounter > 0){
+        setGameCounter(prevState => prevState - 1)
+      }
+    }
+    else if(selectedButton.id > houseButton.id){
+      // win (user:paper, pc:rock || user:scissors, pc:paper)
+      setgameMessage("YOU WIN")
+      setGameCounter(prevState => prevState + 1)
+    }
+  }
+
+  function playAgain(){
+    setSelectedButton(null)
+    setgameMessage("")
+    setHouseButton(null)
+  }
+
+  useEffect(() => {
+    if (selectedButton && houseButton) {
+      console.log("playerId " + selectedButton.id)
+      console.log("houseId " + houseButton.id)
+      determineWinner();
+    }
+  }, [selectedButton, houseButton]);
+  
   return (
     <div className='flex flex-col items-center gap-8 bg-gradient-to-br from-blue-950 via-blue-800 to-blue-950 h-screen'>
       <div className="flex justify-between border-gray-500 border-2 sm:w-[60%] rounded-xl mx-2 mt-2 sm:mt-16">
         <img src={logo} alt="logo" className="ml-4 object-contain max-h-[90%] self-center" />
         <div className="flex flex-col items-center bg-white w-[160px] m-3 py-4 rounded-lg">
           <h3 className="font-bold text-gray-800">S C O R E</h3>
-          <h3 className="text-6xl font-bold text-gray-800">12</h3>
+          <h3 className="text-6xl font-bold text-gray-800">{gameCounter}</h3>
         </div>
       </div>
       <div className="flex gap-10">
@@ -52,8 +98,8 @@ function App() {
                 </div>
               </div>
               <div className={`hidden sm:flex flex-col items-center justify-center gap-4 ${houseButton === null ? "opacity-0" : ""}`}>
-                <h2 className='text-white text-6xl text-center font-bold'>YOU WIN</h2>
-                <button className='bg-white p-2 rounded-xl w-[80%] text-blue-950 font-bold'>PLAY AGAIN</button>
+                <h2 className='text-white text-6xl text-center font-bold'>{gameMessage}</h2>
+                <button className='bg-white p-2 rounded-xl w-[80%] text-blue-950 font-bold' onClick={playAgain}>PLAY AGAIN</button>
               </div>
               <div className={`flex justify-center`}>
                 <div className='flex flex-col gap-6 items-center justify-between'>
@@ -64,8 +110,8 @@ function App() {
               </div>
             </div>
             <div className={`sm:hidden flex flex-col items-center justify-center gap-4 ${houseButton === null ? "opacity-0" : ""}`}>
-                <h2 className='text-white text-6xl text-center font-bold'>YOU WIN</h2>
-                <button className='bg-white p-2 rounded-xl px-16 text-blue-950 font-bold'>PLAY AGAIN</button>
+                <h2 className='text-white text-6xl text-center font-bold'>{gameMessage}</h2>
+                <button className='bg-white p-2 rounded-xl px-16 text-blue-950 font-bold' onClick={playAgain}>PLAY AGAIN</button>
             </div>
           </div>
         }
--- src/components/PlayButton.jsx
+++ src/components/PlayButton.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 function PlayButton(props) {
-  function selectButton(){
+  function clickButton(){
     if(props.selectedButton === null){
-      props.selectButton(props.id)
+      props.clickButton(props.id)
     }
   }
 
   return (
     <div className={`flex rounded-full items-center justify-center w-[150px] h-[150px] sm:w-[200px] sm:h-[200px] ${props.color} shadow-2xl shadow-black cursor-pointer`}
-         onClick={selectButton}
+         onClick={clickButton}
     >
         <div className='flex rounded-full items-center justify-center p-4 sm:p-10 w-[110px] h-[110px] sm:w-[150px] sm:h-[150px] bg-white shadow-inner shadow-black'>
             <img src={props.image} alt="image" className=''/>
